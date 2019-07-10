ad3c430bdf675b0633c36874dd233f6d4f02f1e6
Kevin <yonathan.kevin20@gmail.com>
2019-07-10 16:20:11 +0700

    Updates

---
+++ precache-manifest.3fd76c5796b7a189db8954a362fb62f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9294d49578614ff2b1ae4aa5665d6ab",
+    "url": "/react-first/index.html"
+  },
+  {
+    "revision": "ebfe89ce6fd92bde0155",
+    "url": "/react-first/static/css/main.e96f5903.chunk.css"
+  },
+  {
+    "revision": "1dce0f0574d32da87f42",
+    "url": "/react-first/static/js/2.027cfc7f.chunk.js"
+  },
+  {
+    "revision": "ebfe89ce6fd92bde0155",
+    "url": "/react-first/static/js/main.afa14917.chunk.js"
+  },
+  {
+    "revision": "4b0a6ef6f7219535661b",
+    "url": "/react-first/static/js/runtime~main.ca757f83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-first/static/media/logo.5d5d9eef.svg"
+  }
+]);
